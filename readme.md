f6bc2ab730d160fb32243c5e8458ddbcbb797d94
wenlong <530675800@qq.com>
2020-11-08 14:28:48 +0800

    环境搭建

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Pixi.js Demo
+
+## 环境工具
+[Pixi.js](https://www.pixijs.com/)  
+[parcel2](https://v2.parceljs.org/)  
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import * as PIXI from 'pixi.js'
+
+const app = new PIXI.Application()
+
+// 追加画布到页面
+document.body.appendChild(app.view)
+
